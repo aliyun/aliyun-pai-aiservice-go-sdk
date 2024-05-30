84cfcadd46e5bf0116f391b759b2c9eb7a048259
丁静 <bruceding.jing@alibaba-inc.com>
2024-05-30 18:16:07 +0800

    Fix test case

--- test/llm_test.go
+++ test/llm_test.go
@@ -9,9 +9,11 @@ import (
 
 func TestLLMEvalJudger(t *testing.T) {
 	client := sdk.NewClient(host, appId, token)
+	content := `[{"mode":"single","type":"json","json":{"question":"请介绍下你自己","answer":"我是一个人工智能助手，由 OpenAI 设计并开发，旨在帮助用户解答问题、提供信息、学习语言和执行各种语言处理任务。"}}]
+	`
 	message := []model.LlmEvalJudgerRequestMessages{{
 		Role:    "user",
-		Content: "[\n{\n\"mode\": \"single\",\n\"type\": \"json\",\n\"json\": {\n\"question\": \"请介绍下你自己\",\n\"answer\": \"我是一个人工智能助手，由 OpenAI 设计并开发，旨在帮助用户解答问题、提供信息、学习语言和执行各种语言处理任务。\"\n}\n}\n]",
+		Content: content,
 	}}
 	response, err := client.LLMApi.Create("", "Themis-1.0", message)
 
